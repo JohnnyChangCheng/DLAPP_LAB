65ddc6cb5115455ad407f0972a779dfd1348b216
ChangCheng <37439139+JohnnyChangCheng@users.noreply.github.com>
2020-10-02 22:44:09 +0800

    Resize function

--- lab1-1/lab1-1.py
+++ lab1-1/lab1-1.py
@@ -1,4 +1,5 @@
 import cv2
+import math
 import numpy as np
 
 def splitRGB(img):
@@ -13,9 +14,46 @@ def splitHSV(img):
 
 	return H_map, S_map, V_map
 
-def resize(img, size):
 
-	return img_t
+def bilinear_resize(image, height, width):
+
+	img_height, img_width = image.shape[:2]
+
+	resized = np.zeros((height, width, 3), np.uint8)
+	
+	x_ratio = float(img_width - 1) / float(width - 1)
+	y_ratio = float(img_height - 1) / float(height - 1)
+	print( x_ratio)
+	print( y_ratio )
+	for i in range(height):
+		for j in range(width):
+
+			x_l, y_l = math.floor(x_ratio * j), math.floor(y_ratio * i)
+			x_h, y_h = math.ceil(x_ratio * j), math.ceil(y_ratio * i)
+	
+			x_weight = (x_ratio * j) - x_l
+			y_weight = (y_ratio * i) - y_l
+	
+			a = image[y_l, x_l]
+			b = image[y_l, x_h]
+			c = image[y_h, x_l]
+			d = image[y_h, x_h]
+	
+			pixel = a * (1 - x_weight) * (1 - y_weight) \
+				+ b * x_weight * (1 - y_weight) + \
+				c * y_weight * (1 - x_weight) + \
+				d * x_weight * y_weight
+	
+			resized[i][j] = pixel
+	
+	return resized
+
+def resize(img, size):
+	height = int(np.around(img.shape[0] * size))
+	weight = int(np.around(img.shape[1] * size))
+	print("The image size weigh {weight} and heigh {height}".format(weight=weight, height=height))
+
+	return bilinear_resize(img, height, weight)
 
 class MotionDetect(object):
 	"""docstring for MotionDetect"""
